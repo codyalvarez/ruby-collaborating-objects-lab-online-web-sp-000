0da5b592da8a9931f68cb06fd6c9113b7f35be60
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-27 21:39:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,8 @@ class Song
     song
   end
 
-  def artist_name(name)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
     artist_name = name.to_s
     song = Artist.find_or_create_by_name(name)
     artist_name.song
