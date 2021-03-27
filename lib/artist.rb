92653ea1a66b64f2969a152631fdf4b8c3c5f925
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-27 21:22:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,12 +5,12 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
+    # @@all << self
   end
 
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
 
   def add_song(song)
     @songs << song
